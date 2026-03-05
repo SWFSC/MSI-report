55f53cff2feb242d6aaa7c64f44a33473741fcad
Jeff Moore <jeff.e.moore@noaa.gov>
2026-03-05 10:14:54 -0800

    Code edit for creating data object 'x'
    Fields were added to  HCMSI_Records_SWFSC_Main.xlsx (by JM and AC in 2026).  Code added to remove these new fields in creating the data object 'x'

--- R/AppendixTable.R
+++ R/AppendixTable.R
@@ -7,9 +7,11 @@ load("SeriousInjuryReport.RData")
 
 # data frame 'x' should be a UTF-8 comma-delimited file to work with flextable
 
-include.cols <- names(x)[-1]
+#include.cols <- names(x)[-1]
+include.cols <- names(x)[c(-1,-2,-3,-c(19:24))] # JEM: remove columns that Alex or I added to HCMSI_Records_SWFSC_Main.xlsx but that don't align with shortNames in next row
 shortNames <- c("Species", "Stock", "YYYY", "MM", "DD", "Source", "County", "State", "Initial", "Narrative", "Final", "SI.Criteria", "MSI.Value", "LOF", "PBR")
-x <- x[-1]
+#x <- x[-1]
+x <- x[c(-1,-2,-3,-c(19:24))] # JEM: remove columns in x that don't align with those specified by shortNames
 names(x) <- shortNames
 
 set_flextable_defaults(na_str = "NA", nan_str = "NaN")
@@ -28,6 +30,6 @@ doc <- body_add_flextable(doc, value = A1, align = "left")
 print(doc, target = "Appendix_1.docx")
 
 # Once in Word:
- # - Layout/autofit/autofit to contents
+ # - Layout/autofit/autofit to contents (change to Landscape)
  # - Add Table header from placeholder page in HCM_SI_Report.docx
 # Note carriage returns in Narrative can mess up Word table formatting
